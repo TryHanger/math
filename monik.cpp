81c6b64718a2729b378945323ce787acc5858794
TryHanger <150839191+TryHanger@users.noreply.github.com>
2023-11-15 01:51:18 +0600

    Update monik.cpp

--- monik.cpp
+++ monik.cpp
@@ -12,6 +12,10 @@ int main(int argc, char const *argv[]) {
       std::cout << "Ты шо борзый? Максимальное кол-во чисел в числе: " << names.size() << '\n';
       return -1;
    }
+
+
+
+   
    for (size_t i = 0; i < s_number.size(); i++) {
       std::cout << names[i] << " цифра числа: " << s_number[i] << '\n';
    }
